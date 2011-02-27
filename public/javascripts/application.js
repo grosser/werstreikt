26630f8a50c46c317edb6584860131656079b987
grosser <grosser.michael@gmail.com>
2011-02-27 17:29:47 +0100

    add facebook login

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -19,3 +19,70 @@ String.prototype.truncate = function(to_length){
 function googleNewsFeed(query){
   return "http://news.google.de/news?pz=1&cf=all&ned=de&hl=de&cf=all&output=rss&q="+query.replace(' ','+')
 }
+
+function fillWithFeedEntries(selector, query){
+  google.load("feeds", "1");
+
+  $(function(){
+    var source = googleNewsFeed(query)
+    var items = 3;
+    var feed = new google.feeds.Feed(source);
+    feed.load(function(result) {
+      if (result.error){console.log(result.error)};
+      var $feed = $(selector);
+      for (var i = 0; i <= items; i++) {
+        var entry = result.feed.entries[i];
+        var $content = $(entry.content)
+        $feed.append(
+          '<tr>' +
+            '<td>' +
+            $content.find('td font')[0].innerHTML +
+            '</td>' +
+            '<td>' +
+            $content.find('div')[1].innerHTML +
+            '</td>' +
+            '</tr>'
+          );
+      }
+    });
+  });
+}
+
+FBLoginData = null;
+function withFB(callback){
+  if(FBLoginData) return callback(FBLoginData);
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId: FBAppId,
+      status: true,
+      cookie: true
+    });
+
+    // try to get login info -> no popup
+    FB.getLoginStatus(function(response){
+      if(response){
+        FBLoginData = response.session;
+        callback(FBLoginData);
+      } else {
+        // force login via popup
+        FB.login(function(response){
+          if(response.session){
+            FBLoginData = response.session;
+            callback(login);
+          } else {
+            alert('Login failed');
+          }
+        });
+      }
+    });
+  };
+
+  // load facebook js
+  (function() {
+    var e = document.createElement('script'); e.async = true;
+    e.src = document.location.protocol +
+      '//connect.facebook.net/de_DE/all.js';
+    document.getElementById('fb-root').appendChild(e);
+  }());
+}
