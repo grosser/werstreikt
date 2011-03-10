a7bf2deb44822a1b33653b1d8592d481a75afa7c
grosser <grosser.michael@gmail.com>
2011-03-10 08:00:45 +0100

    add a like button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,9 @@ module ApplicationHelper
   def is_owner?(object)
     current_user.try(:is_owner?, object)
   end
+
+  def like_button(url, options={})
+    width = options[:width] || 450
+    raw %Q{<iframe src="http://www.facebook.com/plugins/like.php?href=#{url}&amp;layout=standard&amp;show_faces=true&amp;width=#{width}&amp;action=like&amp;font&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:#{width}px; height:80px;" allowTransparency="true"></iframe>}
+  end
 end
