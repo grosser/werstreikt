724227915e9779f1c257cf460c0d2864b50edb41
grosser <grosser.michael@gmail.com>
2011-03-12 09:17:06 +0100

    date field with autocomplete feel weird

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -2,6 +2,6 @@ module FormHelper
   def date_text_field(f, field)
     value = f.object[field]
     value = value.strftime('%Y-%m-%d') if value
-    f.text_field field, :class => :datepicker, :value => value
+    f.text_field field, :class => :datepicker, :value => value, :autocomplete => :off
   end
 end
